5a83a0b794afb61ecc5995290718a69d41c78d18
Vibhu-gslab <109593615+Vibhu-gslab@users.noreply.github.com>
2025-02-28 11:40:57 +0530

    Update python-avd/pyavd/_eos_designs/structured_config/underlay/router_pim_sparse_mode.py
    Co-authored-by: Mahesh Kumar <122076792+MaheshGSLAB@users.noreply.github.com>

--- python-avd/pyavd/_eos_designs/structured_config/underlay/router_pim_sparse_mode.py
+++ python-avd/pyavd/_eos_designs/structured_config/underlay/router_pim_sparse_mode.py
@@ -45,7 +45,7 @@ class RouterPimSparseModeMixin(Protocol):
                 continue
 
             # Anycast-RP using PIM (default)
+            other_anycast_rp_addresses = EosCliConfigGen.RouterPimSparseMode.Ipv4.AnycastRpsItem.OtherAnycastRpAddresses()
             for node in rp_entry.nodes:
-                self.structured_config.router_pim_sparse_mode.ipv4.anycast_rps.obtain(rp_entry.rp).other_anycast_rp_addresses.append_new(
-                    address=get(self.shared_utils.get_peer_facts(node.name), "router_id", required=True)
-                )
+                other_anycast_rp_addresses.append(EosCliConfigGen.RouterPimSparseMode.Ipv4.AnycastRpsItem.OtherAnycastRpAddressesItem(address=get(self.shared_utils.get_peer_facts(node.name), "router_id", required=True)))
+            self.structured_config.router_pim_sparse_mode.ipv4.anycast_rps.append_new(address=rp_entry.rp,other_anycast_rp_addresses=other_anycast_rp_addresses)
